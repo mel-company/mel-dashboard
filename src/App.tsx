4225ea116b591d08b121ae7b289926b176ab28c1
bnjamel <malfaris4771@gmail.com>
2025-12-09 11:49:35 +0300

    setup custom font

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="h-[100dvh] flex items-center justify-center w-[100dvw]">
-      <h1>Hello World</h1>
+    <div className="font-bold h-[100dvh] flex items-center justify-center w-[100dvw]">
+      <h1>مرحباً بك</h1>
     </div>
   );
 }
