3b4733443fd4c55efe41cd7f7d24e8e871e6168d
hassan <hassan.21.adnan@gmail.com>
2025-12-10 15:27:33 +0300

    fix the bug

--- src/pages/customer/Customers.tsx
+++ src/pages/customer/Customers.tsx
@@ -9,7 +9,6 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import {
