444f60f58b34906c5ebdbe67eb42f1f9c75de18f
Ali Hammad <hey@alihammad.net>
2026-06-27 10:42:38 +0300

    chore: change ThemeProvider default theme from system to light and remove extra whitespace in usePage hook

--- src/hooks/pages/index.tsx
+++ src/hooks/pages/index.tsx
@@ -3,11 +3,9 @@ import { pages } from '@/utils/pages';
 import { useLocation } from 'react-router-dom'
 
 export const usePage = () => {
-
     const location = useLocation();
     const pathname = location.pathname
     const currentPage = pages?.find(page => page.slug && pathname.includes(page.slug))
-
 
     return {
         pages,
